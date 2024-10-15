16c53fc23140fd5bb3bfda18a91b3bff0c7d62d8
MountainMan12 <pawan.verma@elucidata.io>
2024-10-15 13:57:29 +0530

    update path

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import os
 from svs_processor.processor import SVSProcessor
 
 if __name__ == "__main__":
-    # URL to download the SVS file if it's not present
+    images_dir = '../images/'
+
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+
+    wsi_filename = 'TCGA-02-0010-01Z-00-DX4.07de2e55-a8fe-40ee-9e98-bcb78050b9f7.svs'
+
     wsi_url = 'https://data.kitware.com/api/v1/file/5899dd6d8d777f07219fcb23/download'
 
-    # Test SVS file
-    wsi_filename = 'TCGA-02-0010-01Z-00-DX4.07de2e55-a8fe-40ee-9e98-bcb78050b9f7.svs'
-    output_dir = 'output_directory'
+    output_dir = '../output_directory'
 
-    processor = SVSProcessor(wsi_filename, output_dir, thumbnail_size=(200, 200), level=0, wsi_url=wsi_url)
+    processor = SVSProcessor(os.path.join(images_dir, wsi_filename), output_dir, thumbnail_size=(200, 200), level=0, wsi_url=wsi_url)
     processor.process()
+
